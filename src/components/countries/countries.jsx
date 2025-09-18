3b6403e9d7d46c3607899c52cca6ef39716a7590
sohid7254 <sohidameen321@gmail.com>
2025-09-18 19:45:41 +0600

    first commit

---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Al Countries Are Here</h1>
+        </div>
+    );
+};
+
+export default Countries;
